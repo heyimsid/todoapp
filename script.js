3dd970cc62955433b4c2b370c2cd0243eb53b7f8
Sid <152196280+heyimsid@users.noreply.github.com>
2025-06-18 21:27:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function addTask(){
     const taskList=document.getElementById("taskList");
     const taskTest=taskInput.ariaValueMax.trim();
     if(taskTest==="")return;
-        const listItem=document.createElement("listItem");
+        const listItem=document.createElement("li");
         listItem.textContent=taskTest;
         taskList.appendChild(listItem);
         taskInput.value='';
